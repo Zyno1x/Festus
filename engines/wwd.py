852c936430bae4a1f921186fe262f251c5bea8bb
Zyno1x <76989992+Zyno1x@users.noreply.github.com>
2023-03-13 18:26:10 +0530

    Updated Wake word detector to use PvRecorder For Audio Input

--- engines/wwd.py
+++ engines/wwd.py
@@ -1,27 +1,33 @@
-import struct
-import pyaudio
+#import struct
+#import pyaudio
 import pvporcupine
+from pvrecorder import PvRecorder
 
 porcupine = None
-pa = None
-audio_stream = None
+recorder = None
+#pa = None
+#audio_stream = None
 
 try:
   porcupine = pvporcupine.create(access_key='wc2loO2+wQFfHyzgIV0Pc0NECYrt3hMUYKw4yxVitZUfIMs4e9brEw==',
-                                 keyword_paths=["lib/Felix_en_linux_v2_1_0.ppn"])
+                                 keyword_paths=["lib/Robin_en_raspberry-pi_v2_1_0.ppn"])
 
-  pa = pyaudio.PyAudio()
+  recorder = PvRecorder(device_index=0, frame_length=porcupine.frame_length)
+  recorder.start()
 
-  audio_stream = pa.open(
-      rate=porcupine.sample_rate,
-      channels=1,
-      format=pyaudio.paInt16,
-      input=True,
-      frames_per_buffer=porcupine.frame_length)
+  #pa = pyaudio.PyAudio()
+
+  # audio_stream = pa.open(
+  #    rate=porcupine.sample_rate,
+  #    channels=1,
+  #    format=pyaudio.paInt16,
+  #    input=True,
+  #    frames_per_buffer=porcupine.frame_length)
 
   def detect():
-    pcm = audio_stream.read(porcupine.frame_length)
-    pcm = struct.unpack_from("h" * porcupine.frame_length, pcm)
+    #pcm = audio_stream.read(porcupine.frame_length)
+    #pcm = struct.unpack_from("h" * porcupine.frame_length, pcm)
+    pcm = recorder.read()
 
     keyword_index = porcupine.process(pcm)
 
@@ -33,11 +39,14 @@ try:
     if porcupine is not None:
       porcupine.delete()
 
-    if audio_stream is not None:
-      audio_stream.close()
+    if recorder is not None:
+      recorder.delete()
 
-    if pa is not None:
-      pa.terminate()
+    # if audio_stream is not None:
+    #  audio_stream.close()
+
+    # if pa is not None:
+    #  pa.terminate()
 
 except:
   pass
